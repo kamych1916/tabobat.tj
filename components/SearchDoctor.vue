76a5717332295eac1bae91d83139d879ba121baa
kamych1916 <kamych1916@gmail.com>
2020-08-07 11:41:02 +0300

    add nuxt.js and v-bootstrap

---
+++ components/SearchDoctor.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="search-board">
+        <div class="inner">
+            <div>
+                <h1>Врачи</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.search-board .inner{
+    padding-top: 30px;
+}
+
+</style>
